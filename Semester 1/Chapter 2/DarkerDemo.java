ac1cebfb1a7a5ebd6ea69c14658cbf975ce9cfec
Nastassja Motro <nastassjamotro7@gmail.com>
2017-09-18 21:14:40 -0600

    Update DarkerDemo.java

--- Semester 1/Chapter 2/DarkerDemo.java
+++ Semester 1/Chapter 2/DarkerDemo.java
@@ -1,1 +1,20 @@
+// Nastassja Motro 9/18/17
 
+import java.awt.Color;
+
+public class BrigtherDemo {
+  public static void main(String[] args) {
+    Color myColor = new Color(50, 100, 150);
+    Color brighterColor = myColor.brighter();
+    Color darkerColor = myColor.darker();
+    System.out.println("Red - ");
+    System.out.println(darkerColor.getRed());
+    System.out.println(darkerColor.getRed());
+    System.out.println("Green - ");
+    System.out.println(brighterColor.getGreen());
+    System.out.println("Blue - ");
+    System.out.println(brighterColor.getBlue());
+
+  }
+
+}
