4707fa61fb5d29585cb457bd1704add3c5c765f9
Nastassja Motro <nastassjamotro7@gmail.com>
2017-09-18 21:17:45 -0600

    Create DieSimulator.java

---
+++ Semester 1/Chapter 2/DieSimulator.java
@@ -0,0 +1,10 @@
+// Nastassja Motro 9/18/17
+
+import java.util.Random;
+
+public class DieSimulator {
+  public static void main(String[] args) {
+    Random generator = new Random();
+    System.out.println(generator.nextInt((6) + 1);
+  }
+}
