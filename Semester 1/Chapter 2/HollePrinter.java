e25d45dba0951c4808568f6b52e7863dce4205f8
Nastassja Motro <nastassjamotro7@gmail.com>
2017-09-18 21:06:46 -0600

    Update HollePrinter.java

--- Semester 1/Chapter 2/HollePrinter.java
+++ Semester 1/Chapter 2/HollePrinter.java
@@ -1,5 +1,11 @@
 // Nastassja Motro 9/18/17
 
 public class HollePrinter {
-
+  public static void main(String[] args) {
+    String testString = "Hello World.";
+    String replacedString = testString.replaceAll("o", "~");
+    String replacedString = testString.replaceAll("e", "o");
+    String replacedString = testString.replaceAll("~", "e");
+    System.out.println(testString);
+  }
 }
