be1977aa09643cc8c7528da2a041eceaf11f025d
Nastassja Motro <nastassjamotro7@gmail.com>
2017-09-25 19:03:18 -0600

    Update ColorNameViewer.java

--- Semester 1/Chapter 2/ColorNameViewer.java
+++ Semester 1/Chapter 2/ColorNameViewer.java
@@ -1,13 +1,22 @@
 // Nastassja Motro 09/24/17
 
+/* P2.10: Write a graphics program that draws 12 strings, one each for the 12 standard colors (except Color.WHITE), each in its own
+color. Porivde a class ColorNameViewer and a class ColorNameComponent. */
+
 import javax.swing.JFrame;
+import javax.swing.JComponent;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Line2D;
+import java.awt.geom.Line2D.Double;
 
 public class ColorNameViewer{
   public static void main(String[] args) {
     JFrame frame = new JFrame();
     
     frame.setSize(400, 400);
-    frame.setTitle("ColorNameViewer");
+    frame.setTitle("Color Name Viewer");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     
     ColorNameComponent component = new ColorNameComponent();
@@ -15,17 +24,8 @@ public class ColorNameViewer{
     
     frame.setVisible(true);
   }
-}
 
-import javax.swing.JComponent;
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-
-import java.awt.geom.Line2D;
-import java.awt.geom.LIne2D.Double;
-
-public class ColorNameComponent extends JComponent {
+public static class ColorNameComponent extends JComponent {
   public void paintComponent(Graphics g) {
     Color [] colors = {
       Color.BLACK,
@@ -46,7 +46,8 @@ public class ColorNameComponent extends JComponent {
     
     for(int i=0; i<colors.length; i++) {
       g2.setColors(colors[i]);
-      g2.draw(newLine2D.Double(i, 0, i, 10));
+      g2.draw(newLine2D.Double(i, 50, i, 100));
     }
   }
+ }
 }
