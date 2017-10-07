8cb4426a46782453290ed7c8365b7a90f205aecf
Nastassja Motro <nastassjamotro7@gmail.com>
2017-10-07 11:47:11 -0600

    Update UnitTestCounter.java

--- Semester 1/Chapter 3/UnitTestCounter.java
+++ Semester 1/Chapter 3/UnitTestCounter.java
@@ -1,1 +1,12 @@
 // Nastassja Motro
+
+public class UnitTestCounter {
+  public static void main(String[] args) {
+    Counter tally = new Counter();
+    tally.click();
+    tally.click();
+    int result = tally.getValue();
+    System.out.println("result: " + result);
+    tally.reset();
+  }
+}
