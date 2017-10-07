185c03dd4c3384a9ee05cd78142fbf9dbf268af6
Nastassja Motro <nastassjamotro7@gmail.com>
2017-10-07 11:49:12 -0600

    Update UnitTestBankAccount.java

--- Semester 1/Chapter 3/UnitTestBankAccount.java
+++ Semester 1/Chapter 3/UnitTestBankAccount.java
@@ -1,13 +1,13 @@
 public class UnitTestBankAccount {
 	public static void main(String[] args) {
 		BankAccount account1 = new BankAccount();
-		BankAccount account2 = new BankAccount(500);
+		BankAccount account2 = new BankAccount(600);
 
-		account1.deposit(20);
-		System.out.println("Balance predicted for first account: 20.")
+		account1.deposit(100);
+		System.out.println("Balance predicted for first account: 100.")
 		System.out.println("Actual Balance: " + account1.getBalance());
-		account2.withdraw(200);
-		System.out.println("Balance predicted for second account: 300.")
+		account2.withdraw(400);
+		System.out.println("Balance predicted for second account: 200.")
 		System.out.println("Actual Balance: " + account2.getBalance());
 	}
 }
