8b5bea9bdb9cfcb77fdfc8890fb9096aa4f72c03
Nastassja Motro <nastassjamotro7@gmail.com>
2017-11-29 12:46:02 -0700

    Update Card.java

--- Semester 2/Chapter 7/Card.java
+++ Semester 2/Chapter 7/Card.java
@@ -1,19 +1,19 @@
 // Nastassja Motro 11/27/17
 
 public class Card {
-  String cardSuit;
-  String cardRank;
+  String suit;
+  String rank;
   
-  public Card(String suit, String rank) {
-    cardSuit = suit;
-    cardRank = rank;
+  public Card(String s, String r) {
+    suit = s;
+    rank = r;
   }
   
   public String getSuit() {
-    return cardSuit;
+    return suit;
   }
   
   public String getRank() {
-    return cardRank;
+    return rank;
   }
 }
